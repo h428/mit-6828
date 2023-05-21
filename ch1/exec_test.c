b0a8b6e25bc5b5315f8ae1e1d70e9c5e82f51d9f
hao <lyinghao@126.com>
2023-05-21 19:51:21 +0800

    init commit

---
+++ ch1/exec_test.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <sys/wait.h>
+
+int main() {
+
+    char *argv[3];
+
+    argv[0] = "echo";
+    argv[1] = "hello";
+    argv[2] = 0;
+
+    execv("/bin/echo", argv);
+    printf("exec error\n");
+
+    return 0;
+}
